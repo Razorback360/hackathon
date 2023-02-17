c756ec0fc94e11af07e10ebc28a4a867d8fd58bb
OmarMahmoud20 <omar.abdullgawad@gmail.com>
2023-02-18 01:18:27 +0300

    redo-login-page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,90 @@
 import Head from 'next/head'
-import { useState } from 'react'
-import { useRouter } from 'next/router'
+import React, { useState } from 'react';
+import { Transition } from '@headlessui/react';
+import { EyeIcon, EyeSlashIcon, AtSymbolIcon } from '@heroicons/react/24/outline'
 
-export default function Home() {
+
+function App() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [keepMeSignedIn, setKeepMeSignedIn] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    // handle login logic here
+  };
+
   return (
-    <>
-    <Head>
-        <title>KFUPM Login</title>
-        <link rel="stylesheet" href="C:\Users\User\hackathon\src\styles\globals.css"/>
-      </Head>
-    <div className="flex flex-col items-center justify-center h-screen bg-image bg-[#3c4a3e]">
-      <div className='z-0 opacity-90 absolute bg-[#3c4a3e] w-full h-full'></div>
-      <form className="z-20 bg-white  p-6 rounded-lg shadow-md w-1/4">
-        <img className=" mx-auto mb-4 w-[90%] h-16" src="KFUPM-logo.png" alt="KFUPM Logo"/>
-        <div className="mb-1 bg-green-600 w-[25%] h-3 fixed left-[37.5%]"></div>
-        <h1 className="text-2xl font-medium pt-6  mb-4">Sign in to your account</h1>
-        <div className="mb-4 ">
-          <label className="block w-4/6 mb-1  font-medium text-gray-500 dark:text-gray-500" for="email">Email</label>
-          <input className="block mx-auto rounded-md bg-green-50 border border-green-500 text-green-900 w-full p-2 placeholder-green-700" type="email" id="email" name="email" required/>
-        </div>
-        <div className="mb-6">
-          <label className=" block w-4/6 mb-1  font-medium  text-gray-500 dark:text-gray-500" for="password">Password</label>
-          <input className="block mx-auto rounded-md bg-green-50 border border-green-500 text-green-900 w-full p-2 placeholder-green-700" type="password" id="password" name="password" required/>
-        </div>
-        <div className="flex items-center mb-4">
-          <input className="mt-2 w-4 h-4 text-green-600  bg-green-600 focus:ring-green-500 focus:ring-2  border-gray-300 rounded" type="checkbox" id="keep-me-signed" name="keep-me-signed"/>
-          <label className="text-sm ml-2" for="keep-me-signed">Keep me signed in</label>
-        </div>
-        <button className="mx-auto  block  transform duration-300 bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-2 px-4 border border-green-500 rounded-md">Sign in</button>
-      </form>
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-green-500 to-teal-300">
+      <img src="/Hexagon.png" alt="Background Image" className="w-full h-full object-cover absolute inset-0" />
+      <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        <img src="/logo.png" alt="KFUPM Logo" className="w-32 mx-auto mb-4" />
+        <h1 className="text-2xl font-bold text-green-600 mb-4">EXAMIfY login</h1>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block font-semibold text-gray-600 mb-2">Email</label>
+            <div className="relative">
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="block w-full border border-gray-400 py-2 px-3 rounded-lg focus:outline-none focus:ring-green-600 focus:border-green-600"
+              required
+            />
+            <button
+                type="button"
+                className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-2 text-gray-500 hover:text-gray-600 focus:outline-none"
+                disabled
+              >
+                <AtSymbolIcon className="h-5 w-5" />
+              </button>
+              </div>
+                      </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block font-semibold text-gray-600 mb-2">Password</label>
+            <div className="relative">
+              <input
+                type={showPassword ? 'text' : 'password'}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="block w-full border border-gray-400 py-2 px-3 rounded-lg focus:outline-none focus:ring-green-600 focus:border-green-600 "
+                required
+              />
+              <button
+                type="button"
+                className="absolute right-0 top-1/2 transform -translate-y-1/2 mr-2 text-gray-500 hover:text-gray-600 focus:outline-none"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? <EyeSlashIcon className="h-5 w-5" /> : <EyeIcon className="h-5 w-5" />}
+              </button>
+            </div>
+          </div>
+          <div className="flex items-center mb-4">
+            <input
+              type="checkbox"
+              id="keepMeSignedIn"
+              checked={keepMeSignedIn}
+              onChange={() => setKeepMeSignedIn(!keepMeSignedIn)}
+              className="h-4 w-4 text-green-600 focus:ring-green-500 border-gray-300 rounded"
+            />
+            <label htmlFor="keepMeSignedIn" className="ml-2 block font-medium text-gray-700">
+              Keep me signed in
+            </label>
+          </div>
+          <button
+            type="submit"
+            className="sign-in-button w-full py-2 px-4 rounded-lg text-white font-semibold bg-gradient-to-br from-green-500 to-teal-400 focus:outline-none transition transform duration-300 ease-in-out"
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
     </div>
-    </>
-  )
+    
+
+  );
 }
+
+export default App;
